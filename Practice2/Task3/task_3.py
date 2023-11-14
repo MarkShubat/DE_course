af6439ba3f2e99361a106376028214975a220d5c
Марк <markshubat@gmail.com>
2023-11-15 00:35:51 +0500

    залил исправления второй практики в 3 и 5 задании

--- Practice2/Task3/task_3.py
+++ Practice2/Task3/task_3.py
@@ -12,18 +12,22 @@ for item in json_data:
     price = item["price"]
     if name in result:
         data = result[name]
-        data["average_price"] = (data["average_price"] + price) / 2
+        data["count"]+=1
+        data["summ"] += price
         data["max_price"] = max(data["max_price"], price)
         data["min_price"] = min(data["min_price"], price)
     else:
-        result[name] = {"average_price": price, "max_price": price, "min_price": price}
-
+        result[name] = {"average_price": price, "max_price": price, "min_price": price, "count": 1, "summ": int(price)}
+result1 ={}
+for item in result:
+    result[item]["average_price"] = result[item]["summ"] / result[item]["count"]
+    result1[item] = {"average_price": result[item]["average_price"], "max_price": result[item]["max_price"], "min_price": result[item]["min_price"]}
 file = open("t3_result.json", "w")
-json.dump([{"name": name, **data} for name, data in result.items()], file, indent=2)
+json.dump([{"name": name, **data} for name, data in result1.items()], file, indent=2)
 file.close()
 
 file = open("t3_result.msgpack", "wb")
-file.write(msgpack.packb(result))
+file.write(msgpack.packb(result1))
 file.close()
 
 print("Размер оригинального файла: " + str(os.path.getsize('t3_result.json')))
--- Practice2/Task5/task_5.py
+++ Practice2/Task5/task_5.py
@@ -5,6 +5,14 @@ import os
 
 result = dict()
 data = pd.read_csv('titanic.csv')
+
+dataframe = pd.DataFrame(data)
+dataframe.to_json('titanic.json')
+dataframe.to_pickle('titanic.pkl')
+file = open('titanic.msgpack', 'wb')
+file.write(msgpack.packb(dataframe.to_dict()))
+file.close()
+
 num = ['Survived', 'Pclass', 'Age', 'Siblings/Spouses Aboard', 'Parents/Children Aboard', 'Fare']
 lit = ['Name', 'Sex']
 
@@ -16,19 +24,25 @@ def procNums(result, data, columns):
       'AVG': data[column].mean(),
       'SUM': data[column].sum(),
       'STD': data[column].std(),
+      'FRQ': "Doesn't count for this value"
     }
 
 def procLits(result, data, columns):
   for column in columns:
     result[column] = {
+      'MAX': "Doesn't count for this value",
+      'MIN': "Doesn't count for this value",
+      'AVG': "Doesn't count for this value",
+      'SUM': "Doesn't count for this value",
+      'STD': "Doesn't count for this value",
       'FRQ': data[column].value_counts().to_dict()
     }
-
 procNums(result, data, num)
 procLits(result, data, lit)
 
 df = pd.DataFrame(result)
 df.to_json('t5_result.json')
+
 df.to_csv('t5_result.csv')
 df.to_pickle('t5_result.pkl')
 
@@ -36,7 +50,7 @@ file = open('t5_result.msgpack', 'wb')
 file.write(msgpack.packb(df.to_dict()))
 file.close()
 
-print("Размер json файла: " + str(os.path.getsize('t5_result.json')))
-print("Размер csv файла: " + str(os.path.getsize('t5_result.csv')))
-print("Размер pkl файла: " + str(os.path.getsize('t5_result.pkl')))
-print("Размер msgpack файла: " + str(os.path.getsize('t5_result.msgpack')))
+print("Размер json файла: " + str(os.path.getsize('titanic.json')))
+print("Размер csv файла: " + str(os.path.getsize('titanic.csv')))
+print("Размер pkl файла: " + str(os.path.getsize('titanic.pkl')))
+print("Размер msgpack файла: " + str(os.path.getsize('titanic.msgpack')))
